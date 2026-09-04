aafb9dd20ca74774adc018d29ab3d5a00f1db1fb
trybotgf12-lgtm <trybotgf12@gmail.com>
2026-09-05 01:51:59 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,8 +3,12 @@ import os
 API_ID = int(os.environ.get("API_ID", "0"))
 API_HASH = os.environ.get("API_HASH", "")
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-SESSION_STRING = os.environ.get("SESSION_STRING", "")  # assistant account, joins VC
+SESSION_STRING = os.environ.get("SESSION_STRING", "")
 OWNER_ID = int(os.environ.get("OWNER_ID", "0"))
 PORT = int(os.environ.get("PORT", "8080"))
+MONGO_URL = os.environ.get("MONGO_URL", "")
 
-# sanity checks happen at startup in main.py
+_sudo_raw = os.environ.get("SUDO_USERS", "")
+SUDO_USERS = [int(x.strip()) for x in _sudo_raw.split(",") if x.strip()]
+if OWNER_ID and OWNER_ID not in SUDO_USERS:
+    SUDO_USERS.append(OWNER_ID)
