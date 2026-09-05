b62e4c45ff963e8ee890388470273bb0a24e1089
trybotgf12-lgtm <trybotgf12@gmail.com>
2026-09-05 09:08:41 +0530

    Update ytdl.py

--- ytdl.py
+++ ytdl.py
@@ -14,7 +14,6 @@ YDL_OPTS = {
     "default_search": "ytsearch",
     "geo_bypass": True,
     "nocheckcertificate": True,
-    "extractor_args": {"youtube": {"player_client": ["web"]}},
 }
 
 if os.path.exists(SECRET_COOKIES_PATH):
